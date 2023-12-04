c335aa648fe72b9588ef16c74e122341eb872abe
Ewen Sellitto <ewen.sellitto@epitech.eu>
2023-12-04 22:29:45 +0100

    feat: added colors :D

--- install.sh
+++ install.sh
@@ -2,22 +2,26 @@
 
 PACKAGE="lsd bat exa ripgrep fzf tmux zsh tig neofetch btop"
 BREW_PACKAGE="scc"
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+NC='\033[0m'
 
 function add_zsh_plugin() {
     local plugin=$1
     if [ -z "$plugin" ]; then
-        echo "Please specify a plugin name"
+        echo -e "${RED}Please specify a plugin name${NC}"
         return 1
     fi
 
     local zshrc="$HOME/.zshrc"
     if [ ! -f "$zshrc" ]; then
-        echo ".zshrc not found"
+        echo "${RED}.zshrc not found${NC}"
         return 1
     fi
 
     if grep -q "plugins=(.* $plugin .*)" "$zshrc"; then
-        echo "Plugin $plugin already added."
+        echo "${YELLOW}Plugin $plugin already added.${NC}"
         return 1
     fi
 
@@ -29,7 +33,7 @@ function add_zsh_plugin() {
 
 function other_install() {
     # oh-my-zsh
-    echo 'istalling oh-my-zsh and plugins'
+    echo -e "${GREEN}istalling oh-my-zsh and plugins${NC}"
     echo 'exit 0' | $(sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     add_zsh_plugin zsh-syntax-highlighting
@@ -38,17 +42,17 @@ function other_install() {
     add_zsh_plugin zsh-autosuggestions
 
     # tmux
-    echo 'installing tmux and plugins'
+    echo -e  "${GREEN}installing tmux and plugins${NC}"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     mkdir -p $HOME/.config/tmux
     cp tmux.conf $HOME/.config/tmux/tmux.conf
 
     # kitty
-    echo 'installing kitty'
+    echo -e "${GREEN}installing kitty${NC}"
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
     # cdsearch
-    echo 'installing cdsearch'
+    echo -e "${GREEN}installing cdsearch${NC}"
     mkdir -p $HOME/.local/bin
     cp cdsearch $HOME/.local/bin/cdsearch
     echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.zshrc
@@ -61,7 +65,7 @@ function install_mac() {
 }
 
 function install_fedora() {
-    sudo dnf install $PACKAGE -y
+    # sudo dnf install $PACKAGE -y
     which brew && brew install $BREW_PACKAGE
     other_install
 }
